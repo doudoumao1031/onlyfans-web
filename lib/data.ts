e693958420ac4553d13c61584f8bf4a08f8dd489
doudou <doudoumao1031@gmail.com>
2024-12-12 16:27:07 +0800

    vercel deploy

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,8 @@ export async function fetchSeeds(
     currentPage: number,
 ){
     console.log('process env', process.env)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/seeds?page=${currentPage}`, {
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/seeds?page=${currentPage}`, {
+    const res = await fetch(`https://onlyfans-demo.vercel.app/api/seeds?page=${currentPage}`, {
         cache: "no-store", // Ensure fresh data on every request
     });
     const { items, hasMore } = await res.json();
