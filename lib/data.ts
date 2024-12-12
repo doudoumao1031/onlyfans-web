0a9b72dd55f849dd8dcde6baec2605853e135b7c
doudou <doudoumao1031@gmail.com>
2024-12-12 16:13:13 +0800

    .env update

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,8 @@
 export async function fetchSeeds(
     currentPage: number,
 ){
-    const res = await fetch(`http://localhost:3000/api/seeds?page=${currentPage}`, {
+    console.log('process env', process.env)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/seeds?page=${currentPage}`, {
         cache: "no-store", // Ensure fresh data on every request
     });
     const { items, hasMore } = await res.json();
