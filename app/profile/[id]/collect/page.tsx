077f605036a0d9455a277a8786a7298daa0d1b6b
qiao <slxnl@gmail.com>
2024-12-18 11:15:42 +0800

    update

---
+++ app/profile/[id]/collect/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/profile/header";
+
+export default function Page (){
+    return <>
+    <div className="profile-content bg-[url('/demo/user_bg.png')]">
+        <Header  title="收藏夹" />
+        {/* <div className="text-xs pl-6 pr-6">
+          各位亲爱的粉丝：感谢有你们的陪伴，今日起订阅老用户一律5折，新用户8折
+        </div> */}
+      </div>
+    </>
+}
+
