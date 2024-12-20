48883b117ad1afa31b2e9f3a9791f548b71582ae
qqm <123@qq.com>
2024-12-20 10:53:28 +0800

    feat: ui lib and page

--- app/profile/[id]/draft/edit/page.tsx
+++ app/profile/[id]/draft/edit/page.tsx
@@ -2,20 +2,26 @@
 import React, {useState} from "react";
 import clsx from "clsx";
 import IconWithImage from "@/components/profile/icon";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
+import {Switch} from "@/components/ui/switch";
+import {useRouter} from "next/navigation";
 
-const ItemEditTitle = ({title}:{title:React.ReactNode}) =>{
+const ItemEditTitle = ({title, showIcon = true}: { title: React.ReactNode, showIcon?: boolean }) => {
     return <div className="flex gap-2.5 items-center">
         <div className="font-bold text-base">{title}</div>
-        <IconWithImage url={"/icons/profile/icon_edit@3x.png"} width={20} height={20} color={'#bbb'}/>
+        {showIcon && <IconWithImage url={"/icons/profile/icon_edit@3x.png"} width={20} height={20} color={'#bbb'}/>}
     </div>
 }
 
 
 export default function Page() {
     const [disabledSubmit] = useState<boolean>(true)
+    const router = useRouter()
     return <div>
         <section className="flex justify-between h-11 items-center pl-4 pr-4 ">
-            <IconWithImage url={"/icons/profile/icon_close@3x.png"} width={24} height={24} color={'#000'}/>
+            <button onTouchEnd={router.back}>
+                <IconWithImage url={"/icons/profile/icon_close@3x.png"} width={24} height={24} color={'#000'}/>
+            </button>
             <button type="button" className={clsx(disabledSubmit ? "text-[#bbb]" : "#000")}>发布</button>
         </section>
 
@@ -36,7 +42,8 @@ export default function Page() {
             </section>
             <section className="mt-2.5 rounded-xl bg-[#F4F5F5] px-3 py-2">
                 <div className="flex gap-2.5 items-center">
-                    <IconWithImage url={"/icons/profile/icon_fans_vote@3x.png"} width={20} height={20} color={'#FF8492'}/>
+                    <IconWithImage url={"/icons/profile/icon_fans_vote@3x.png"} width={20} height={20}
+                                   color={'#FF8492'}/>
                     <span className="font-bold text-main-pink text-base">投票名称</span>
                 </div>
                 <div className="text-xs text-[#999] mt-1.5">截止：2012-01-01 12:12 结束</div>
@@ -44,6 +51,23 @@ export default function Page() {
         </section>
         <section className="pt-5 pb-5 pl-4 pr-4 border-b border-gray-200">
             <ItemEditTitle title={"阅览设置："}/>
+            <RadioGroup defaultValue="option-one" className="mt-2.5">
+                <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="option-one" id="option-one" className="text-main-pink border-main-pink"/>
+                    <label htmlFor="option-one">免费订阅</label>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="option-two" id="option-two"/>
+                    <label htmlFor="option-two">无法浏览-订阅者</label>
+                </div>
+            </RadioGroup>
+        </section>
+        <section className="pt-5 pb-5 pl-4 pr-4 ">
+            <ItemEditTitle showIcon={false} title={'发布通知'}/>
+            <section className="border-b border-gray-200 flex justify-between items-center py-3">
+                <div>订阅者</div>
+                <Switch></Switch>
+            </section>
         </section>
     </div>
 }
--- components/profile/manuscript/manuscript-item.tsx
+++ components/profile/manuscript/manuscript-item.tsx
@@ -1,6 +1,7 @@
 // 稿件
 import Image from "next/image";
 import IconWithImage from "@/components/profile/icon";
+import Link from "next/link";
 
 const ShowNumberWithIcon = ({icon, number}: { icon: string, number: number }) => {
     return <section className="flex justify-center flex-col items-center flex-1">
@@ -23,10 +24,10 @@ const ManuscriptActions = ()=>{
             <IconWithImage url={"/icons/profile/icon_fans_data@3x.png"} width={20} height={20} color={"#777"} />
             <span>数据</span>
         </button>
-        <button className="flex-1 flex gap-2 pt-2.5 pb-2.5 text-main-pink">
+        <Link href={"/profile/123/draft/edit"} className="flex-1 flex gap-2 pt-2.5 pb-2.5 text-main-pink">
             <IconWithImage url={"/icons/profile/icon_edit@3x.png"} width={20} height={20} color={"#FF8492"} />
             <span>编辑</span>
-        </button>
+        </Link>
     </section>
 }
 
