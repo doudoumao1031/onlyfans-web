7cb9ad479d8723147308de06ebf093311928a5a3
qiaoy <slxnl@gmail.com>
2024-12-24 18:28:43 +0800

    add media

--- app/space/[id]/feed/page.tsx
+++ app/space/[id]/feed/page.tsx
@@ -1,15 +1,15 @@
 import { fetchFeeds } from "@/lib/data";
-import Feed, { fakePostData } from "@/components/post/feed";
+import Post from "@/components/post/post"
+import { postData } from "@/components/post/mock"
+
 export default async function Page() {
     const { items, hasMore }: { items: number[], hasMore: boolean } = await fetchFeeds(1);
     return <>
-        <div className="max-w-lg mx-auto grid grid-cols-1 gap-4 mt-4">
-            {items.map((item, index) => (
-                <div key={index} className="w-full">
-                    <Feed data={fakePostData} />
-                </div>
-            ))}
-        </div>
+     <div className="max-w-lg mx-auto grid grid-cols-1 gap-4 mt-4">
+             {items.map((item, index) => (
+               <Post key={index} data={postData} showSubscribe showVote />
+             ))}
+           </div>
         {!hasMore && (
             <div className="text-center mt-4">
                 <p className="text-gray-500">You have reached the end.</p>
