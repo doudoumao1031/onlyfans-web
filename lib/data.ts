1d4db981bb14c0a5dad74aa1bd739e015011ab0b
doudou <doudoumao1031@gmail.com>
2024-12-24 16:17:20 +0800

    build bug fix

--- lib/data.ts
+++ lib/data.ts
@@ -3,8 +3,8 @@ export async function fetchFeeds(
 ) {
     // console.log('process env', process.env)
     // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/feeds?page=${currentPage}`, {
-    const res = await fetch(`http://localhost:3000/api/feeds?page=${currentPage}`, {
-    // const res = await fetch(`https://onlyfans-demo.vercel.app/api/feeds?page=${currentPage}`, {
+    // const res = await fetch(`http://localhost:3000/api/feeds?page=${currentPage}`, {
+    const res = await fetch(`https://onlyfans-demo.vercel.app/api/feeds?page=${currentPage}`, {
         cache: "no-store", // Ensure fresh data on every request
     });
     const {items, hasMore} = await res.json();
