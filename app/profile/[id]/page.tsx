e0cdbef3a520f16dd285de22de9af64571b3a5d3
qqm <123@qq.com>
2024-12-25 10:38:35 +0800

    fix: collect route link

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -22,7 +22,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
               height={22}
           />
         </>} title="My" backIconColor={'#fff'}/>
-        <div className="text-xs pl-6 pr-6">
+        <div className="text-xs pl-6 pr-6 text-white">
           各位亲爱的粉丝：感谢有你们的陪伴，今日起订阅老用户一律5折，新用户8折
         </div>
       </div>
@@ -70,18 +70,20 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
         <div className="pl-4 pr-4">
           <div className="flex justify-between items-center pt-2.5 pb-2.5">
             <h3 className="text-[15px] font-bold">收藏夹</h3>
-            <button className="text-gray-300">&gt;</button>
+            <Link href={`/profile/${id}/collect?type=blog`} className="text-gray-300">
+              <IconWithImage url={"/icons/profile/icon_arrow_right@3x.png"} width={16} height={16} color={'#ddd'} />
+            </Link>
           </div>
 
           <div className="grid grid-cols-2 gap-3">
-            <div className="rounded-xl pt-1.5 pl-4 bg-[url('/icons/profile/bg-collect-blogger.png')] bg-cover">
+            <Link href={`/profile/${id}/collect?type=blog`} className="rounded-xl pt-1.5 pl-4 bg-[url('/icons/profile/bg-collect-blogger.png')] bg-cover">
               <div className="text-xs text-gray-400">博主</div>
               <div className="font-medium text-[34px] ">9999</div>
-            </div>
-            <div className="rounded-xl pt-1.5 pl-4 bg-[url('/icons/profile/bg-collect-posts.png')] bg-cover">
+            </Link>
+            <Link href={`/profile/${id}/collect?type=post`} className="rounded-xl pt-1.5 pl-4 bg-[url('/icons/profile/bg-collect-posts.png')] bg-cover">
               <div className="text-xs text-gray-400">帖子</div>
               <div className="font-medium text-[34px] ">9999</div>
-            </div>
+            </Link>
           </div>
 
           <PostsCard
