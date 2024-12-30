a14954f930a7c392d1f6b4db617524fd66e04a51
qiaoy <slxnl@gmail.com>
2024-12-30 09:58:52 +0800

    change post

--- app/space/[id]/feed/page.tsx
+++ app/space/[id]/feed/page.tsx
@@ -1,19 +1,25 @@
+import FeedList from "@/components/explore/feed-list";
+import { ListEnd } from "@/components/explore/list-states";
+import Post from "@/components/post/post";
 import { fetchFeeds } from "@/lib/data";
-import Post from "@/components/post/post"
-import { postData } from "@/components/post/mock"
+
+export const revalidate = 3600; // Regenerate the page every 3600 seconds
 
 export default async function Page() {
-    const { items, hasMore }: { items: number[], hasMore: boolean } = await fetchFeeds(1);
-    return <>
-     <div className="max-w-lg mx-auto grid grid-cols-1 gap-4 mt-4">
-             {items.map((item, index) => (
-               <Post key={index} data={postData} showSubscribe showVote />
-             ))}
-           </div>
-        {!hasMore && (
-            <div className="text-center mt-4">
-                <p className="text-gray-500">You have reached the end.</p>
+    const { items, hasMore } = await fetchFeeds(1);
+    return (
+        <div className="container h-full w-full mx-auto">
+            <div className="max-w-lg mx-auto grid grid-cols-1 gap-4">
+                {items.map((item, index) => (
+                    <Post
+                        key={`${item.id}-${index}`}
+                        data={item}
+                        showSubscribe
+                        showVote
+                    />
+                ))}
             </div>
-        )}
-    </>
+            {!hasMore && <ListEnd />}
+        </div>
+    );
 }
