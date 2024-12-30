13224ae5d94b5485e4b646e08188706a741c03e3
kepler999 <kepler999@gmail.com>
2024-12-30 15:12:30 +0800

    feat(explore): user/subscribed-drawer

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import Search from "@/components/explore/search";
+import UserCard from "@/components/user/user-card";
+import { users} from "@/mock/user";
+
 export default async function Page(
     props: {
         searchParams?: Promise<{
@@ -9,9 +12,11 @@ export default async function Page(
 ) {
     const searchParams = await props.searchParams;
     const query = searchParams?.search || '';
+    console.log(query)
     // 搜索结果
-    const result = "";
-    // const result = await searchBlog();
+    const result = "1";
+    // const interfactRes = searchPost(query).then((data)=>data.data);
+
     return (
         /** 搜索 */
         <div className="w-full flex flex-col justify-center">
@@ -22,13 +27,29 @@ export default async function Page(
                 )
             }
             {
-                result === "" && <div className="flex flex-col justify-center items-center justify-items-center mt-40">
+                query !== "" && result === "" && <div className="flex flex-col justify-center items-center justify-items-center mt-40">
                     <Image src="/icons/explore/icon_search_null@3x.png" alt="search is null"
                            width={200}
                            height={150}
                     />
                     <span className="mt-6 text-gray-500 text-center">没有搜到相关博主，请尝试别的搜索词</span>
                 </div>
+            }
+            {
+                query !== "" && result !== "" && (
+                    <>
+                        <div className="flex flex-col justify-start px-4 pt-[20px]">
+                            <span className="font-medium text-left text-[#6D7781]">用户</span>
+                            <div className="w-full mt-[10px]">
+                                <UserCard card={users[0]} subscribe={true}/>
+                            </div>
+                        </div>
+                        <hr className="border-t border-gray-200 w-full my-[20px]"></hr>
+                        <div className="flex flex-col justify-start px-4">
+                            <span className="font-medium text-left text-[#6D7781]">博文</span>
+                        </div>
+                    </>
+                )
             }
         </div>
     );
