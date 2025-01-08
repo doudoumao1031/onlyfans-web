2365484ff32b46b14e164b85f6b81e883af5ac13
qiaoy <slxnl@gmail.com>
2025-01-08 14:42:17 +0800

    update

--- app/space/[id]/feed/page.tsx
+++ app/space/[id]/feed/page.tsx
@@ -1,21 +1,12 @@
 import { fetchFeeds } from "@/lib/data"
-import Post from "@/components/post/post"
-import { postData } from "@/components/post/mock"
+import FeedList from "@/components/explore/feed-list"
 
 export default async function Page() {
-  const { items, hasMore }: { items: number[], hasMore: boolean } = await fetchFeeds(1)
-  return (
-    <>
-      <div className="max-w-lg mx-auto grid grid-cols-1 gap-4 mt-4">
-        {items.map((item, index) => (
-          <Post key={index} data={postData} showSubscribe showVote />
-        ))}
-      </div>
-      {!hasMore && (
-        <div className="text-center mt-4">
-          <p className="text-gray-500">You have reached the end.</p>
+    const { items, hasMore } = await fetchFeeds(1)
+
+    return (
+        <div className="container h-full w-full mx-auto">
+            <FeedList initialItems={items} initialHasMore={hasMore} />
         </div>
-      )}
-    </>
-  )
+    )
 }
