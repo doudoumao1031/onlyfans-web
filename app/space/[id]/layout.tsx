f02035bace87b0407ce81baa125ab6f5b08a4747
qiaoy <slxnl@gmail.com>
2025-01-09 16:45:29 +0800

    update

--- app/space/[id]/layout.tsx
+++ app/space/[id]/layout.tsx
@@ -1,23 +1,23 @@
 import TabLink from "@/components/space/tab-link"
 import UserInfo from "@/components/space/userInfo"
 export default async function Layout(
-  props: {
-    children: React.ReactNode;
-    modal: React.ReactNode;
-    params: Promise<{ id: string }>
-  }
+    props: {
+        children: React.ReactNode;
+        modal: React.ReactNode;
+        params: Promise<{ id: string }>
+    }
 ) {
-  const { id } = await props.params
-  return (
-    <>
-      {props.modal}
-      <div className="flex h-screen flex-col w-full justify-start items-center overflow-auto">
-        <UserInfo id={id} />
-        <TabLink id={id} />
-        <div className="grow px-4 py-3 w-full h-3/4">{props.children}</div>
+    const { id } = await props.params
+    return (
+        <>
+            {props.modal}
+            <div className="flex h-screen flex-col w-full justify-start items-center overflow-auto">
+                <UserInfo />
+                <TabLink id={id} />
+                <div className="grow px-4 py-3 w-full h-3/4">{props.children}</div>
 
-      </div>
-      <div id="modal-root" />
-    </>
-  )
+            </div>
+            <div id="modal-root" />
+        </>
+    )
 }
