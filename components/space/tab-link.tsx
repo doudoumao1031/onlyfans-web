b11b7128504f938a9cd299d446a12279701f7ea0
qiaoy <slxnl@gmail.com>
2025-01-10 14:55:04 +0800

    update

--- components/space/tab-link.tsx
+++ components/space/tab-link.tsx
@@ -4,30 +4,30 @@ import clsx from "clsx"
 import { useCallback } from "react"
 import { usePathname } from "next/navigation"
 export default function TabLinks({ id }: { id: string }) {
-  const pathName = usePathname()
-  const pathNameClass = useCallback((href: string) => {
-    if (pathName === href) {
-      return "font-bold text-black "
-    }
-    return "text-[#777] font-normal"
-  }, [pathName])
-  const links = [
-    { name: "帖子", href: `/space/${id}/feed`, num: 9999 },
-    { name: "媒体", href: `/space/${id}/media`, num: 9999 }
-  ]
-  return (
-    <div className="w-full text-center grid grid-cols-2 border-b border-gray-100">
-      {links.map((link) => (
-        <Link
-          prefetch={true}
-          key={link.name}
-          href={link.href}
-          className={clsx("pt-3.5 pb-3.5 text-[20px] relative", pathNameClass(link.href))}
-        >
-          {`${link.name}(${link.num})`}
-          <span className={clsx("absolute left-[50%] bottom-0 h-[3px] rounded-tl-lg rounded-tr-lg bg-black w-[40px] ml-[-20px]", pathName === link.href ? "block" : "hidden")}></span>
-        </Link>
-      ))}
-    </div>
-  )
+    const pathName = usePathname()
+    const pathNameClass = useCallback((href: string) => {
+        if (pathName === href) {
+            return "font-bold text-black "
+        }
+        return "text-[#777] font-normal"
+    }, [pathName])
+    const links = [
+        { name: "帖子", href: `/space/${id}/feed`, num: 9999 },
+        { name: "媒体", href: `/space/${id}/media`, num: 9999 }
+    ]
+    return (
+        <div className="w-full text-center grid grid-cols-2 border-b border-gray-100">
+            {links.map((link) => (
+                <Link
+                    prefetch={true}
+                    key={link.name}
+                    href={link.href}
+                    className={clsx("pt-3.5 pb-3.5 text-[20px] relative", pathNameClass(link.href))}
+                >
+                    {`${link.name}(${link.num})`}
+                    <span className={clsx("absolute left-[50%] bottom-0 h-[3px] rounded-tl-lg rounded-tr-lg bg-black w-[40px] ml-[-20px]", pathName === link.href ? "block" : "hidden")}></span>
+                </Link>
+            ))}
+        </div>
+    )
 }
