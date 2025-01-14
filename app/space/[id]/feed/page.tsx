255eaf083902623ab4e33d3e201d0ca7ddd2b70f
qiaoy <slxnl@gmail.com>
2025-01-14 14:45:58 +0800

    UPDATE

--- app/space/[id]/feed/page.tsx
+++ app/space/[id]/feed/page.tsx
@@ -1,16 +1,12 @@
 import FeedList from "@/components/explore/feed-list"
-import { getMyFeeds } from "@/lib/actions/space/actions"
+import { fetchMyPosts } from "@/lib/actions/space/actions"
 
 export default async function Page() {
-    const res = await getMyFeeds({ page: 1, pageSize: 10, from_id: 1 })
-    if (!res) {
-        throw new Error("error")
-    }
-    const { list, total } = res.data
+    const { items, hasMore } = await fetchMyPosts(1)
 
     return (
         <div className="container h-full w-full mx-auto">
-            <FeedList initialItems={list} initialHasMore={total > 10} />
+            <FeedList initialItems={items} initialHasMore={hasMore} />
         </div>
     )
 }
--- lib/actions/space/actions.ts
+++ lib/actions/space/actions.ts
@@ -1,7 +1,35 @@
-import { CommonPageReq, ENDPOINTS, fetchWithGet, fetchWithPost, PageResponse } from "@/lib";
+import { CommonPageReq, ENDPOINTS, fetchWithGet, fetchWithPost, PageResponse, PostData } from "@/lib";
 import { TFeeListItem } from "./types";
 
 //我的帖子
-export const getMyFeeds = (params: CommonPageReq) => {
-  return fetchWithPost<CommonPageReq, PageResponse<TFeeListItem>>(ENDPOINTS.POST.ME_POSTS, params)
+export const getMyFeeds = (params: CommonPageReq) => fetchWithPost<CommonPageReq, PageResponse<PostData>>(ENDPOINTS.POST.ME_POSTS, params).then((res) => {
+  if (res && res.code === 0) {
+    return res.data
+  } else {
+    return null
+  }
+})
+
+//resetPost
+export const fetchMyPosts = async (page: number, fromId: number = 0, pageSize: number = 10) => {
+  const response = await getMyFeeds({
+    from_id: fromId,
+    page,
+    pageSize: pageSize
+  })
+
+  if (!response) {
+    return {
+      items: [],
+      hasMore: false
+    }
+  }
+
+  const { list, total } = response
+
+  const hasMore = page * pageSize < total
+  return {
+    items: list,
+    hasMore
+  }
 }
