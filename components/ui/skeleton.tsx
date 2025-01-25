3fba07576e506e5a2ace7f087174b6690bc81262
qiaoy <slxnl@gmail.com>
2025-01-25 18:32:52 +0800

    skeleton

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,12 +1,12 @@
 import { cn } from "@/lib/utils"
-
+import "./skeleton.scss"
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn("w-full bg-default rounded-md", className)}
       {...props}
     />
   )
