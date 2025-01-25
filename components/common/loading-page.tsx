96f509afd837d372b2db7687bc78fe79c3adc9ce
kepler999 <kepler999@gmail.com>
2025-01-25 17:09:11 +0800

    feat(explore): loading

---
+++ components/common/loading-page.tsx
@@ -0,0 +1,8 @@
+export default function LoadingPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      {/*<div className="animate-spin rounded-full h-16 w-16 border-b-2 border-gray-900"></div>*/}
+      <img src="/icons/loading.png" alt="loading" className={"h-16 w-16 animate-spin"}/>
+    </div>
+  )
+}
