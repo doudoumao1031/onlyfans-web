a4892a83771d877cf9e9951f413f72affe299f24
qiaoy <slxnl@gmail.com>
2025-02-08 10:19:03 +0800

    update

--- components/post/avatar.tsx
+++ components/post/avatar.tsx
@@ -3,12 +3,14 @@ import LazyImg from "../common/lazy-img"
 
 export default function Avatar({ fileId, width = 16 }: { fileId: string; width?: number }) {
   return (
-    <LazyImg
-      src={buildImageUrl(fileId)}
-      alt=""
-      className={`rounded-full border-2 border-white w-${width} h-${width}`}
-      width={50}
-      height={50}
-    />
+    <div className={`w-${width}`}>
+      <LazyImg
+        src={buildImageUrl(fileId)}
+        alt=""
+        className={`rounded-full border-2 border-white w-${width} h-${width}`}
+        width={50}
+        height={50}
+      />
+    </div>
   )
 }
