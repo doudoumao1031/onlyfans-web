f61c4e81fe745adcea743a90af761bed7fa3d8b4
k9 <k9@gmail.com>
2025-02-13 15:12:25 +0800

    feat(explore): media

--- components/post/media.tsx
+++ components/post/media.tsx
@@ -19,10 +19,12 @@ export default function Media({
 }) {
   const showIds = data.map((v) => v.file_id).join("_")
   return (
-    <div className="grid grid-cols-3 gap-2 relative">
+    <>
       {((post.visibility === 1 && !user.sub) || post.visibility === 2) && (
-        <>
-          <div className="w-full h-full bg-black bg-opacity-[30%] rounded-lg backdrop-blur absolute top-0 left-0 z-[99] flex flex-col items-center justify-center">
+        <div className="w-full h-[200px] relative">
+          <div
+            className="w-full h-full bg-black bg-opacity-[30%] rounded-lg backdrop-blur absolute top-0 left-0 z-[99] flex flex-col items-center justify-center"
+          >
             <IconWithImage
               url="/icons/icon_info_lock_white.png"
               width={32}
@@ -35,35 +37,42 @@ export default function Media({
             className={"aspect-square rounded-md block"}
             src={"/icons/default/img_media_default.png"}
             alt=""
-            width={200}
+            style={{
+              width: "100%",
+              height: "200px"
+            }}
+            width={343}
             height={200}
           />
-        </>
+        </div>
       )}
-      {(post.visibility === 0 || (post.visibility === 1 && user.sub)) &&
-        data.map(({ file_id, file_type, thumb_id }, i) => {
-          return (
-            <Link
-              key={i}
-              href={`/media/${file_type === FileType.Video ? "video" : "image"}/${
-                file_type === FileType.Video ? showIds : showIds + "_" + i
-              }`}
-              className={file_type === FileType.Video ? "col-span-3" : "block"}
-            >
-              {file_type === FileType.Video ? (
-                <VideoPreview fileId={file_id} thumbId={thumb_id} />
-              ) : (
-                <LazyImg
-                  className="aspect-square rounded-md "
-                  src={buildImageUrl(file_id)}
-                  alt=""
-                  width={200}
-                  height={200}
-                />
-              )}
-            </Link>
-          )
-        })}
-    </div>
+      {(post.visibility === 0 || (post.visibility === 1 && user.sub)) && (
+        <div className="grid grid-cols-3 gap-2 relative">
+          {data.map(({ file_id, file_type, thumb_id }, i) => {
+            return (
+              <Link
+                key={i}
+                href={`/media/${file_type === FileType.Video ? "video" : "image"}/${
+                  file_type === FileType.Video ? showIds : showIds + "_" + i
+                }`}
+                className={file_type === FileType.Video ? "col-span-3" : "block"}
+              >
+                {file_type === FileType.Video ? (
+                  <VideoPreview fileId={file_id} thumbId={thumb_id}/>
+                ) : (
+                  <LazyImg
+                    className="aspect-square rounded-md "
+                    src={buildImageUrl(file_id)}
+                    alt=""
+                    width={200}
+                    height={200}
+                  />
+                )}
+              </Link>
+            )
+          })}
+        </div>
+      )}
+    </>
   )
 }
