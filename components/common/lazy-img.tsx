b9492fc0c354e1ccba5bcc43c58c5c732edf7ba9
qqm <123@qq.com>
2025-02-14 18:28:03 +0800

    fix: bugs

--- components/common/lazy-img.tsx
+++ components/common/lazy-img.tsx
@@ -2,17 +2,21 @@
 import { useState } from "react"
 import { Skeleton } from "@/components/ui/skeleton"
 import Image from "next/image"
-import { ComponentProps } from "react"
-type MyImageProps = ComponentProps<typeof Image> & {
-};
+import React, { ComponentProps } from "react"
+
+type MyImageProps = ComponentProps<typeof Image> & {};
 export default function LazyImg(props: MyImageProps): React.ReactNode {
   const [isLoading, setIsLoading] = useState(true)
   return (
-    <div className="relative w-full h-full">
+    <div className="relative" style={{
+      width: props.width,
+      height: props.height
+    }}
+    >
       {
         (isLoading) && <Skeleton className={`${props.className} w-full h-full absolute `}></Skeleton>
       }
-      {props.src && <Image {...props} onLoad={(e) => setIsLoading(false)} />}
+      {props.src && <Image {...props} onLoad={() => setIsLoading(false)}/>}
     </div>
 
   )
