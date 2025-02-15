780a7de46356f95c9cfa762227a1c2b5acd18aa9
doudou <doudoumao1031@gmail.com>
2025-02-15 16:29:27 +0800

    fix: font default color

--- components/common/common-message.tsx
+++ components/common/common-message.tsx
@@ -4,7 +4,7 @@ import IconWithImage from "@/components/profile/icon"
 
 export default function useCommonMessage() {
   const [content, setContent] = useState<React.ReactNode>()
-  const [type, setType] = useState<string | "default" | "success" | "fail">("default")
+  const [type, setType] = useState<string | "default" | "success" | "fail" | "love">("default")
   const [openState, setOpenState] = useState<boolean>(false)
   const [isVisible, setIsVisible] = useState<boolean>(false)
   const [delay, setDelay] = useState<number>(3000)
@@ -44,9 +44,9 @@ export default function useCommonMessage() {
     if (content && openState) {
       return (
         <div
-          className={`absolute z-[9999] left-1/2 -translate-x-1/2 top-[50vh] rounded-full bg-black/80 px-4 py-2 text-white transition-all duration-300 ease-in-out transform ${
+          className={`absolute z-[9999] left-1/2 -translate-x-1/2 top-[50vh] rounded-full px-4 py-2 text-white transition-all duration-300 ease-in-out transform ${
             isVisible ? "opacity-100 scale-100" : "opacity-0 scale-95"
-          }`}
+          } ${type === "love" ? "bg-red" : "bg-black/80"}`}
         >
           {
             type === "default" && (
@@ -57,6 +57,14 @@ export default function useCommonMessage() {
             type === "success" && (
               <div className={"w-36 h-12 flex justify-center items-center"}>
                 <IconWithImage url={"/icons/checkbox_select_white@3x.png"} height={20} width={20}/>
+                <span className={"text-white font-medium text-base"}>{content}</span>
+              </div>
+            )
+          }
+          {
+            type === "love" && (
+              <div className={"w-36 h-12 flex justify-center items-center"}>
+                <IconWithImage url={"/icons/icon_fans_like_highlight@3x.png"} height={20} width={20}/>
                 <span className={"text-white font-medium text-base"}>{content}</span>
               </div>
             )
