632bba76c90e36fab7889977d1b0342ae08baa24
qqm <123@qq.com>
2025-02-24 11:20:29 +0800

    feat: i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,7 @@
-import { useTranslations } from "next-intl"
-import { Link } from "@/i18n/routing"
-
-export default function HomePage() {
-  const t = useTranslations("HomePage")
+export default function Home() {
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
     </div>
   )
 }
--- lib/providers/providers.tsx
+++ lib/providers/providers.tsx
@@ -1,22 +1,26 @@
-"use client"
 
 import React from "react"
 import { GlobalProvider } from "@/lib/contexts/global-context"
 import { MessageProvider } from "@/lib/contexts/message-context"
 import { LoadingProvider } from "@/lib/contexts/loading-context"
+import { getMessages } from "next-intl/server"
+import { NextIntlClientProvider } from "next-intl"
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
-export default function Providers({ children }: ProvidersProps) {
+export default async function Providers({ children }: ProvidersProps) {
+  const messages = await getMessages()
   return (
-    <GlobalProvider>
-      <MessageProvider>
-        <LoadingProvider>
-          {children}
-        </LoadingProvider>
-      </MessageProvider>
-    </GlobalProvider>
+    <NextIntlClientProvider messages={messages}>
+      <GlobalProvider>
+        <MessageProvider>
+          <LoadingProvider>
+            {children}
+          </LoadingProvider>
+        </MessageProvider>
+      </GlobalProvider>
+    </NextIntlClientProvider>
   )
 }
