4e0129438897e6bf0599bb9398a3993d7fd7787d
qqm <123@qq.com>
2025-03-07 11:23:14 +0800

    fix: bugs

--- components/common/lazy-img.tsx
+++ components/common/lazy-img.tsx
@@ -4,13 +4,21 @@ import { Skeleton } from "@/components/ui/skeleton"
 import Image from "next/image"
 import { ComponentProps } from "react"
 import { buildImageUrl } from "@/lib/utils"
-type MyImageProps = ComponentProps<typeof Image> & {}
+import clsx from "clsx"
+
+type MyImageProps = ComponentProps<typeof Image> & {
+  containerAuto?: boolean
+}
 export default function LazyImg(props: MyImageProps): React.ReactNode {
   const [isLoading, setIsLoading] = useState(true)
   return (
-    <div className="relative w-full h-full flex justify-center">
+    <div className={clsx(
+      "relative flex justify-center",
+      !props.containerAuto ? "w-full h-full" :""
+    )}
+    >
       {isLoading && <Skeleton className={`${props.className} w-full h-full absolute `}></Skeleton>}
-      {props.src && <Image {...props} onLoad={() => setIsLoading(false)} />}
+      {props.src && <Image {...props} onLoad={() => setIsLoading(false)}/>}
     </div>
   )
 }
@@ -19,7 +27,7 @@ export function LazyImageWithFileId(props: Omit<MyImageProps, "src"> & { fileId:
   const { fileId } = props
   if (fileId) {
     const src = buildImageUrl(fileId)
-    return <LazyImg {...props} src={src} />
+    return <LazyImg {...props} src={src}/>
   }
   return (
     <Skeleton
