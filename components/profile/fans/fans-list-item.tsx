0e51e3c6d9395f8e65ada6835bc51931c5bfea83
qqm <123@qq.com>
2025-03-17 16:31:44 +0800

    fix: fans user link

--- components/profile/fans/fans-list-item.tsx
+++ components/profile/fans/fans-list-item.tsx
@@ -3,6 +3,7 @@ import { FansFollowItem, FansSubscribeItems } from "@/lib"
 import dayjs from "dayjs"
 import CommonAvatar from "@/components/common/common-avatar"
 import { useTranslations } from "next-intl"
+import { Link } from "@/i18n/routing"
 
 export function FansSubscribe({ data }: {
   data: FansSubscribeItems
@@ -17,16 +18,16 @@ export function FansSubscribe({ data }: {
   }
   return (
     <div className={"flex gap-4 items-center"}>
-      <div className={" shrink-0"}>
+      <Link href={`/space/${data.user.id}/feed`} className={"shrink-0"}>
         <CommonAvatar photoFileId={data.user.photo} size={40}/>
-      </div>
+      </Link>
       <div className={"flex-1 flex justify-between border-b border-[#ddd] py-3 "}>
-        <button className={"text-left"}>
+        <Link href={`/space/${data.user.id}/feed`} className={"text-left"}>
           <div className={"text-base text-[#222] font-medium"}>{data.user.first_name}</div>
           <div
             className={"text-xs text-[#bbb]"}
           >{showTime(data.end_time)}</div>
-        </button>
+        </Link>
         <button className={"shrink-0"}>
           <IconWithImage url={"/icons/profile/icon_chat_gray@3x.png"} width={24} height={24} color={"#777"}/>
         </button>
@@ -37,7 +38,7 @@ export function FansSubscribe({ data }: {
 
 export function FollowedListItem({ data }:{data:FansFollowItem}) {
   return (
-    <div className={"flex gap-4 items-center"}>
+    <Link href={`/space/${data.user.id}/feed`} className={"flex gap-4 items-center"}>
       <div className={"shrink-0"}>
         <CommonAvatar photoFileId={data.user.photo} size={40}/>
       </div>
@@ -49,6 +50,6 @@ export function FollowedListItem({ data }:{data:FansFollowItem}) {
           >{dayjs(data.following_time * 1000).format("YYYY-MM-DD HH:mm")}</div>
         </button>
       </div>
-    </div>
+    </Link>
   )
 }
